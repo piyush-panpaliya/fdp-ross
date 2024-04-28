9df70f6ae2e1d3caf31f8476371a770a0caa1cfd
psh <psh@ubuntu20.myguest.virtualbox.org>
2024-04-28 21:42:49 +0530

    removed build and devel

--- lab1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/psh/Documents/ros/src
-# Build directory: /home/psh/Documents/ros/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("lol")
--- lab1/build/lol/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/psh/Documents/ros/src/lol
-# Build directory: /home/psh/Documents/ros/build/lol
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
